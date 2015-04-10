b98f5340ad1e39eed3e7fe258e313d6012fd4781
Callymanga <anyibaba1@gmail.com>
2015-04-10 13:55:05 +0200

    Update SoftwareLab_1.java
    Trying out here

--- C/SoftwareLab_1/src/softwarelab_1/SoftwareLab_1.java
+++ C/SoftwareLab_1/src/softwarelab_1/SoftwareLab_1.java
@@ -19,6 +19,8 @@ public class SoftwareLab_1 {
         System.out.println("Hello!");
         //prints hello!
         System.out.println("it works!!! Jippi");
+    
+    System.out.println("we are champions");
     }
     
 }
